683788145472e35624c50f92993f4d0b918ff2d2
mend0kuse <vasilievsam03@mail.ru>
2022-09-15 13:03:13 +0800

    1st commit

---
+++ script.js
@@ -0,0 +1,91 @@
+function todo() {
+	let todoInput = document.querySelector('.todo__input');
+	let todoAdd = document.querySelector('.todo__add');
+	let todoContent = document.querySelector('.todo__content');
+
+	let keys = Object.keys(localStorage);
+	for (let key of keys) {
+		let div = document.createElement('div');
+		div.innerHTML = localStorage.getItem(key);
+		let text = div.querySelector('p')
+		if (text.dataset.chec == 'true') {
+			add(text.textContent, 'true')
+		} else {
+			add(text.textContent)
+		}
+	}
+
+	function add(val = todoInput.value, che = 'false') {
+		let note = document.createElement('div');
+		let noteCheck = document.createElement('input');
+		let noteText = document.createElement('p');
+		let noteDelete = document.createElement('button');
+
+		note.classList = 'todo__note';
+
+		if (che == 'true') {
+			note.classList.add('note-completed');
+			noteCheck.checked = 'true';
+		}
+
+		noteCheck.type = 'checkbox';
+		noteCheck.addEventListener('change', (e) => {
+			let parent = noteCheck.closest('.todo__note')
+			parent.classList.toggle('note-completed')
+			localStorage.removeItem(`${parent.querySelector('p').textContent}`)
+
+			if (noteCheck.checked) {
+				parent.querySelector('p').dataset.chec = 'true'
+				console.log('axd');
+			} else {
+				parent.querySelector('p').dataset.chec = 'false'
+			}
+
+			localStorage.setItem(parent.querySelector('p').textContent, parent.innerHTML)
+		})
+
+		noteText.textContent = val;
+		noteText.contentEditable = 'true';
+
+		noteDelete.textContent = 'Удалить';
+		noteDelete.addEventListener('click', (e) => {
+			let parent = noteDelete.closest('.todo__note')
+			todoContent.removeChild(parent)
+			localStorage.removeItem(`${parent.querySelector('p').textContent}`)
+			if (todoContent.children.length == 1) {
+				todoContent.firstChild.remove()
+			}
+		})
+
+		note.append(noteCheck)
+		note.append(noteText)
+		note.append(noteDelete)
+
+
+		if (todoContent.children.length < 1) {
+			clear()
+		}
+
+		todoContent.append(note)
+		localStorage.setItem(noteText.textContent, note.innerHTML)
+	}
+
+	function clear() {
+		let clear = document.createElement('button')
+		clear.classList = 'clear btn'
+		clear.textContent = 'Очистить'
+		clear.addEventListener('click', (e) => {
+			localStorage.clear()
+			while (todoContent.lastChild) {
+				todoContent.lastChild.remove()
+			}
+		})
+		todoContent.prepend(clear)
+	}
+
+	todoAdd.addEventListener('click', (e) => {
+		add()
+	})
+}
+
+todo()
